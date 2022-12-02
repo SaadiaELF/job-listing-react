9eef2c005f4d6898714a71d613ca56d27a7f14f2
SaadiaElf <saadiaelfekak@gmail.com>
2022-12-02 23:40:08 +0000

    Styling

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React from "react";
 import data from "./data.json";
-import "./App.css";
 import Card from "./components/Card";
+import Container from "./components/Container";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {data.map((job, i) => {
-        return (
-          <Card
-            key={i}
-            imgSrc={job.logo}
-            imgAlt={job.company}
-            company={job.company}
-            position={job.position}
-            featured={job.featured}
-            new={job.new}
-            time={job.postedAt}
-            contract={job.contract}
-            location={job.location}
-            role={job.role}
-            level={job.level}
-            languages={job.languages}
-            tools={job.tools}
-          />
-        );
-      })}
+      <Container>
+        {data.map((job, i) => {
+          return (
+            <Card
+              key={i}
+              imgSrc={job.logo}
+              imgAlt={job.company}
+              company={job.company}
+              position={job.position}
+              featured={job.featured}
+              new={job.new}
+              time={job.postedAt}
+              contract={job.contract}
+              location={job.location}
+              role={job.role}
+              level={job.level}
+              languages={job.languages}
+              tools={job.tools}
+            />
+          );
+        })}
+      </Container>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,21 +3,23 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className="card">
-      <img
-        className="card__img"
-        src={props.imgSrc}
-        alt={props.imgAlt}
-      />
+      <img className="card__img" src={props.imgSrc} alt={props.imgAlt} />
       <div className="card__body">
         <h3 className="card__title">
-          {props.company}
-          {props.featured ? <span className="status">Featured</span> : ""}
-          {props.new ? <span className="status">New</span> : ""}
+          <span className="span-title">{props.company}</span>
+          {props.new ? <span className="status status--new">New!</span> : ""}
+          {props.featured ? (
+            <span className="status status--featured">Featured</span>
+          ) : (
+            ""
+          )}
         </h3>
         <h2 className="card__subtitle">{props.position}</h2>
         <div className="card__text">
           <span className="info">{props.time}</span>
+          <span className="info">.</span>
           <span className="info">{props.contract}</span>
+          <span className="info">.</span>
           <span className="info">{props.location}</span>
         </div>
       </div>
